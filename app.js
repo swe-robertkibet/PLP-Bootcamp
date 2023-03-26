607093d01437de9df8dc0e92ac30d20217102322
swe-robertkibet <swe.robertkibet@gmail.com>
2023-03-26 16:36:18 +0300

    Animation

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-//Elements
-var toggle_btn = document.querySelector(".toggle-btn");
-var big_wrapper = document.querySelector(".big-wrapper");
+
+var toggle_btn;
+var big_wrapper;
+
+function declare(){
+    toggle_btn = document.querySelector(".toggle-btn");
+    big_wrapper = document.querySelector(".big-wrapper");
+}
+
 const main =document.querySelector("main");
+
+declare();
 
 let dark = false;
 
@@ -21,6 +29,21 @@ function toggleAnimation() {
     clone.classList.add("copy");
     main.appendChild(clone);
 
+    document.body.classList.add("stop-scrolling");
+
+    clone.addEventListener("animationend", () =>{
+        document.body.classList.remove("stop-scrolling");
+        big_wrapper.remove();
+        clone.classList.remove("copy");
+
+        declare();//reset variables
+        events();
+    });
+
 }
 
-toggle_btn.addEventListener('click', toggleAnimation);
+function events(){
+    toggle_btn.addEventListener('click', toggleAnimation);
+}
+
+events();
