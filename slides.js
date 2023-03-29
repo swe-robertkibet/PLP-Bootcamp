5320c3d9fe35d4324e805e1c3ef6221a9d79373e
swe-robertkibet <swe.robertkibet@gmail.com>
2023-03-30 01:45:10 +0300

    Done with Slides

---
+++ slides.js
@@ -0,0 +1,63 @@
+var toggle_btn;
+var big_wrapper;
+
+function declare(){
+    toggle_btn = document.querySelector(".toggle-btn");
+    big_wrapper = document.querySelector(".big-wrapper");
+}
+
+const main =document.querySelector("main");
+
+declare();
+
+let dark = false;
+
+function toggleAnimation() {
+    //Clonenwrapper
+    dark = !dark;
+    let clone = big_wrapper.cloneNode(true);
+    if(dark === true){
+        clone.classList.remove("light");
+        clone.classList.add("dark");
+    }
+    else{
+        clone.classList.remove("dark");
+        clone.classList.add("light");
+    }
+    
+    clone.classList.add("copy");
+    main.appendChild(clone);
+
+    document.body.classList.add("stop-scrolling");
+
+    clone.addEventListener("animationend", () =>{
+        document.body.classList.remove("stop-scrolling");
+        big_wrapper.remove();
+        clone.classList.remove("copy");
+
+        declare();//reset variables
+        events();
+    });
+
+}
+
+function events(){
+    toggle_btn.addEventListener('click', toggleAnimation);
+}
+
+events();
+
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach((panel) => {
+   panel.addEventListener('click', () =>{
+       removeActiveClasses()
+       panel.classList.add('active')
+   })
+})
+
+function removeActiveClasses(){
+   panels.forEach((panel) => {
+       panel.classList.remove('active')
+   })
+}
